6fda8c53c082e089e036307965fd36f7e680ebc5
William H <william.hermozo@protonmail.com>
2025-05-27 11:09:56 +0200

    hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
