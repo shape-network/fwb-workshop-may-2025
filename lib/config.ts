50f27134c44f570444e8a5dfa92b34f4bf4336a3
William H <william.hermozo@protonmail.com>
2025-05-27 12:15:31 +0200

    config

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,7 @@
+import { shape, shapeSepolia } from 'viem/chains';
+
 export const config = {
-  chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
+  chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID) as typeof shape.id | typeof shapeSepolia.id,
   alchemyKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY as string,
   walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string,
 } as const;
