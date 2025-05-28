c89fb8887053c45f9fd0434bc89e54aa8bdaed61
William H <william.hermozo@protonmail.com>
2025-05-28 12:25:10 +0200

    fix import

--- components/assembly-item.tsx
+++ components/assembly-item.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardTitle } from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { ScrollArea } from '@/components/ui/scroll-area';
